c50e879324180682cbb9d71ee8ca80f6c1a9aec2
Xingjun Ma <xingjunm@student.unimelb.edu.au>
2018-09-30 18:58:07 +1000

    talk map

--- talkmap/org-locations.js
+++ talkmap/org-locations.js
@@ -1,22 +1,47 @@
 var addressPoints = [
   [
-    "Berkeley CA, USA",
-    37.8708393,
-    -122.2728638
+    "Tokyo, Japan",
+    35.6924407,
+    139.755834
   ],
   [
-    "London, UK",
-    51.5073219,
-    -0.1276473
+    "Stockholm, Sweden",
+    59.2773099,
+    18.0129369
   ],
   [
-    "San Francisco, California",
-    37.7792808,
-    -122.4192362
+    "Vancouver, Canada",
+    49.2883336,
+    -123.1143303
   ],
   [
-    "Los Angeles, CA",
-    34.0543942,
-    -118.2439408
+    "Quebec City, Canada",
+    46.8564962,
+    -71.62189
+  ]
+  [
+    "Melbourne, Australia",
+    -37.8261252,
+    144.9509188
+  ]
+  [
+    "Rome, Italy",
+    41.9102416,
+    12.2557964
+  ]
+  [
+    "vatican City",
+    41.9019742,
+    12.4540805
+  ]
+  [
+    "Shanghai, China",
+    31.1752667,
+    121.3110513
+  ]
+  [
+    "Beijing, China",
+    39.9385876,
+    115.8369507
   ]
 ];

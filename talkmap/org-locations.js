8da475e8cb8e012e721f2b3bb376bb86e4044e17
Xingjun Ma <xingjunm@student.unimelb.edu.au>
2018-09-30 19:12:52 +1000

    places visited

--- talkmap/org-locations.js
+++ talkmap/org-locations.js
@@ -18,27 +18,27 @@ var addressPoints = [
     "Quebec City, Canada",
     46.8564962,
     -71.62189
-  ]
+  ],
   [
     "Melbourne, Australia",
     -37.8261252,
     144.9509188
-  ]
+  ],
   [
     "Rome, Italy",
     41.9102416,
     12.2557964
-  ]
+  ],
   [
     "vatican City",
     41.9019742,
     12.4540805
-  ]
+  ],
   [
     "Shanghai, China",
     31.1752667,
     121.3110513
-  ]
+  ],
   [
     "Beijing, China",
     39.9385876,
